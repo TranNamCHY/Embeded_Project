83636e5f8c97e8642b9ffd92f935857747da04e7
TranNamCHY <135244822+TranNamCHY@users.noreply.github.com>
2023-06-06 22:45:17 +0700

    Add files via upload

---
+++ Objects/project.d
@@ -0,0 +1,12 @@
+.\objects\project.o: project.c
+.\objects\project.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\project.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\project.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\project.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\project.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\project.o: .\RTE\Device\MKL46Z256VLL4\MKL46Z4.h
+.\objects\project.o: C:\Users\MY PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\project.o: C:\Users\MY PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\project.o: C:\Users\MY PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\project.o: C:\Users\MY PC\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\project.o: .\RTE\Device\MKL46Z256VLL4\system_MKL46Z4.h
